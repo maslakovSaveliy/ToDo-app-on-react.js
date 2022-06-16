9eb574e315fc8a2394f874375dbf7c1337dcedfa
Saveliy Maslakov <93765422+maslakovSaveliy@users.noreply.github.com>
2022-06-17 01:58:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,19 +10,13 @@ import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
-// Импортируем наш новый кастомный хук
 import { useFetching } from "./hooks/useFetching";
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  // Теперь мы можем удалить состояние isPostsLoading
-  // Сразу же воспользуемся нашим новым хуком
-  // Деструктуризируем принимаемые нами функцию, состояние загрузки постов и ошибку
-  // Потом вызываем useFetching и внутрь него передаём колбек
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    // Внутри нашего передаваемого колбека мы реализовываем функционал, который у нас уже был в функции fetchPosts, которую можно также удалять
     const posts = await PostService.getAll();
     setPosts(posts);
   });
@@ -46,8 +40,6 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {/*Сразу же обрабатываем некоторую ошибку*/}
-      {/*Если у нас в postError что-то есть, то будем выводить следующий заголовок*/}
       {postError && <h1>Произошла ошибка ${postError}</h1>}
       {isPostsLoading ? (
         <div
