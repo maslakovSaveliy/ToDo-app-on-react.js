c13857ce8d973a178f180bd85faacc854d96ac76
sssaaaaaammm <saveliymaslakov969@gmail.com>
2022-06-28 17:42:18 +0300

    routes fix

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Posts from "../pages/Posts";
-import About from "../pages/About";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
+import { routes } from "../router/routes";
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<h1>Home page</h1>} />
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/error" element={<Error />} />
+      {routes.map((route) => (
+        <Route element={<route.component />} path={route.path} />
+      ))}
       <Route path="*" element={<Navigate to="/error" />} />
     </Routes>
   );
---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+import About from "../pages/About";
+import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
+import Posts from "../pages/Posts";
+export const routes = [
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/posts",
+    component: Posts,
+  },
+  {
+    path: "/posts/:id",
+    component: PostIdPage,
+  },
+  {
+    path: "*",
+    component: Error,
+  },
+];
